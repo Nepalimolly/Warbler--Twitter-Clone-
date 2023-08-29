415c531af246e295ea1aaa5b63324261a57bf43a
Nepalimolly <121688585+Nepalimolly@users.noreply.github.com>
2023-08-29 15:43:13 -0500

    fixed User profile page, Adding location, Bio & header image url

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
+from pdb import set_trace
 
 from flask import Flask, render_template, request, flash, redirect, session, g
 from sqlalchemy.exc import IntegrityError
+from flask_debugtoolbar import DebugToolbarExtension
 
 from forms import UserAddForm, LoginForm, MessageForm
 from models import db, connect_db, User, Message
@@ -9,13 +11,18 @@ from models import db, connect_db, User, Message
 CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
+app.debug = True
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Lukadon1996$@localhost/warbler'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = "it's a secret"
+
+toolbar = DebugToolbarExtension(app)
+
 
 with app.app_context():
     connect_db(app)
@@ -79,6 +86,7 @@ def signup():
             return render_template('users/signup.html', form=form)
 
         do_login(user)
+        
 
         return redirect("/")
 
